99cab7b6104ad4d637920b1c821e12486b9c7fd8
Gabriel9164 <gabrielitoox123@gmail.com>
2025-05-07 15:42:22 -0600

    Pensé que iba avanzado rapido

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -18,7 +18,9 @@ def create_app():
          origins=[
              "http://localhost:5173",
              "http://127.0.0.1:5173",
-             "http://192.168.1.64:5176"
+             "http://192.168.1.64:5176",
+             "http://10.177.73.2:5173"
+
          ],
          supports_credentials=True,
          allow_headers=["Content-Type", "Authorization"],
